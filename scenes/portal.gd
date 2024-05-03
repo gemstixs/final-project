577b998f3e62ffee0146ac6c832620601d71f337
gemstixs <155918699+gemstixs@users.noreply.github.com>
2024-05-03 12:45:42 -0400

    I GOT MY STUFF TO WORK
    not really I had to change things to diverting scenes but oh well!

--- scenes/portal.gd
+++ scenes/portal.gd
@@ -1,7 +1,5 @@
-extends Window
+extends Node2D
 
-func _on_portal_pressed():
-	self.show() 
+func _on_portal_pressed() -> void:
+	get_tree().change_scene_to_file("res://portal.tscn")
 
-func _on_window_close_requested():
-	self.hide()
